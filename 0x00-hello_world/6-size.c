d7488c6af9aab951602962d388f1b781d6413e21
Hajar-hil <hilali.hajar21@gmail.com>
2023-03-16 12:50:00 -0700

    Printing Sizes of different primitive types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %d",sizeof(char));
+printf("Size of string: %d",sizeof(string));
+printf("Size of integer: %d",sizeof(int));
+return(0);
+}
