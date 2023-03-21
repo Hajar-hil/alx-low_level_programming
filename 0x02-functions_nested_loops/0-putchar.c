2b64839bdb01c8a5a6801ecd9b576ee07714a7a3
Hajar-hil <hilali.hajar21@gmail.com>
2023-03-21 12:33:26 -0700

    fix code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main : entry point
  * 
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-char *p = "_putchar";
-
-while(*p)
-{
-_putchar(*p);
-p++;
-}
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
 _putchar("\n");
 return (0);
 }
